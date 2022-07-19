6bb0b3b3fc8dcbf9a93133a11c86fecf3a48219b
Alvarotloc <alvarotloc@gmail.com>
2022-07-19 14:59:17 +0200

    ModalCard editada para textos en inglés y español

--- src/components/ModalCard.jsx
+++ src/components/ModalCard.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // Importamos de react router la posibilidad de hacer una navegación una vez pase algo
 import useEventos from "../hooks/useEventos";
 import useFormData from "../hooks/useFormData";
+import useIdioma from "../hooks/useIdioma";
 import styles from "../styles/ModalCard.module.css";
 const ModalCard = () => {
+  const [textos, setTextos] = useState([]);
   const { setModal, objetoModal, setObjetoModal, handleEliminar } = useEventos(); //Desestructuramos lo necesario de el context de Eventos
+  const {isSpanish} = useIdioma();
   const { setObjetoEditar } = useFormData(); //Desestructuramos lo necesario de el context de FormData
   const { nombre, fecha, participantes, descripcion, _id } = objetoModal; // Desestructuramos los datos de objeto modal que es el que se llena cuando haces click en un día que tiene un evento asignado
   const navigate = useNavigate(); //Definimos la constante navigate para hacer la navegación una vez pase algo
+
+  useEffect(() => {
+    if(isSpanish){
+      setTextos(['Editar','Borrar']);
+      return;
+    }
+    setTextos(['Edit','Delete']);
+  })
+
   const handleEditar = () => { //Definimos la función que se ejectutará cuando se edite un evento para ir al formulario y que éste tenga acceso al evento que queremos editar
     setObjetoEditar(objetoModal);
     setModal(false);
@@ -27,10 +41,10 @@ const ModalCard = () => {
           <p>Participantes: {participantes}</p>
           <div className={styles.botones}>
             <button type="button" onClick={handleEditar}>
-              Editar
+              {textos[0]}
             </button>
             <button type="button" onClick={() => handleEliminar(_id)}> {/* HandleEliminar nos llega desde el context y necesita de un id */}
-              Borrar
+              {textos[1]}
             </button>
           </div>
         </section>
